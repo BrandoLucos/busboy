28af32a811cb4becdcd20be002668294a14502d4
Awesome <bc.lucas89@gmail.com>
2016-08-03 13:40:13 -0400

    Made many changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var lightItem = 15
-var mediumItem = 20
-var heavyItem = 35
+// var lightItem = 15
+// var mediumItem = 20
+// var heavyItem = 35
 
 // Wait for DOM to be ready
 $(function() {
@@ -9,19 +9,6 @@ $(function() {
   stopwatch.reset();
 });
 
-var game = {
-  calcScore: function() {
-    if (stopwatch.elapsedTime < 500) {
-      alert('You are fast');
-    }
-    else if (stopwatch.elapsedTime < 1000) {
-      alert('Not bad');
-    }
-    else {
-      alert('Too slow');
-    }
-  }
-};
 
 var stopwatch = {
 
@@ -29,13 +16,13 @@ var stopwatch = {
     this.startTime = null;
     this.endTime = null;
     this.elapsedTime = 0;
-    this.$stopWatchButton.text('Start');
+    this.$stopWatchButton.text('Start then stop on "3"');
     this.mode = 'new';
     this.render();
   },
 
   start: function() {
-    this.$stopWatchButton.text('Stop');
+    this.$stopWatchButton.text('Pull');
     this.mode = 'started';
     this.startTime = Date.now();
     var that = this;
@@ -77,4 +64,47 @@ var stopwatch = {
     }
   }
 };
+var total = 0;
+
+function wonItAll() {
+  if(total >= 500) {
+  alert('YOU WON IT ALL, KID!');
+  }
+};
+
+var game = {
+  calcScore: function() {
+
+      if (stopwatch.elapsedTime < 3151 && stopwatch.elapsedTime > 2849) {
+        total = (total + 100);
+        document.getElementById('result').innerHTML = total;
+      alert('Hey, good job that was a near perfect pull');
+      wonItAll();
+
+}
+    else if (stopwatch.elapsedTime < 2849 && stopwatch.elapsedTime > 2500) {
+        total = (total + 60);
+        document.getElementById('result').innerHTML = total;
+        divOne = document.getElementById('jug')
+         divOne.style.visibility = 'hidden';
+      alert('Too fast, youre making a mess kid!');
+      wonItAll();
+
+}
+    else if (stopwatch.elapsedTime > 3151 && stopwatch.elapsedTime < 3500) {
+        total = (total + 40);
+        document.getElementById('result').innerHTML = total;
+      alert('Too slow your gonna break something!');
+      wonItAll();
+}
+    else if (stopwatch.elapsedTime > 3500 || stopwatch.elapsedTime < 2500) {
+        total = (total - 100);
+        document.getElementById('result').innerHTML = total;
+      alert('I dont know if you caught a snag or what but its coming out of your check');
+      wonItAll();
+}
+}
+}
+
+//var scoreboard=
 
