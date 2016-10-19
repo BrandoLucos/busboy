ae0e5a4f0025a5531b53491f3f1d6da14595bc6b
Awesome <bc.lucas89@gmail.com>
2016-10-18 23:58:13 -0400

    Made it so that items on the table hide

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var stopwatch = {
         this.$stopWatchButton.text('Start then stop on "3" secs');
         this.mode = 'new';
         this.render();
+        document.getElementById('table').hidden=false;
     },
 
     start: function() {
@@ -61,13 +62,7 @@ var stopwatch = {
 };
 var total = 0;
 
-
-
-// var divOne = document.getElementById(2);
-// divOne.style.display=("hidden");
-
-// var divTwo = document.getElementByClass(2);
-// divOne.style.display=("show");
+// Actual game, prompts, and score calculator
 
 var game = {
     calcScore: function() {
@@ -75,37 +70,61 @@ var game = {
         if (stopwatch.elapsedTime < 3151 && stopwatch.elapsedTime > 2849) {
             total = (total + 100);
             document.getElementById('result').innerHTML = total;
-            alert('Hey, good job that was near perfect\!');
+            $.notify('Hey, good job that was near perfect\!');
 
         } else if (stopwatch.elapsedTime < 2849 && stopwatch.elapsedTime > 2500) {
             total = (total + 60);
             document.getElementById('result').innerHTML = total;
+            document.getElementById('jug').hidden=true;
+            document.getElementById('vase').hidden=true;
+            document.getElementById('I').hidden=false;
+            document.getElementById('J').hidden=false;
+            document.getElementById('F').hidden=false;
+            document.getElementById('B').hidden=false;
             alert('Too fast, youre making a mess kid!');
-            // divOne = document.getElementById('jug' , 'vase')
-            //divTwo = document.getElementByClass('X3')
 
         } else if (stopwatch.elapsedTime > 3151 && stopwatch.elapsedTime < 3500) {
             total = (total + 40);
             document.getElementById('result').innerHTML = total;
+            document.getElementById('fork1').hidden=true;
+            document.getElementById('fork2').hidden=true;
+            document.getElementById('knife1').hidden=true;
+            document.getElementById('knife2').hidden=true;
+            document.getElementById('A').hidden=false;
+            document.getElementById('C').hidden=false;
+            document.getElementById('E').hidden=false;
+            document.getElementById('H').hidden=false;
             alert('Too slow your gonna break something!');
-            //divOne = document.getElementById('fork1', 'fork2' , 'knife1', 'knife2')
-            //divTwo = document.getElementByClass('X1')
+
 
         } else if (stopwatch.elapsedTime > 3500 || stopwatch.elapsedTime < 2500) {
             total = (total - 100);
             document.getElementById('result').innerHTML = total;
+            document.getElementById('fork1').hidden=true;
+            document.getElementById('fork2').hidden=true;
+            document.getElementById('knife1').hidden=true;
+            document.getElementById('knife2').hidden=true;
+            document.getElementById('plate1').hidden=true;
+            document.getElementById('plate2').hidden=true;
+            document.getElementById('jug').hidden=true;
+            document.getElementById('vase').hidden=true;
+            document.getElementById('cup1').hidden=true;
+            document.getElementById('cup2').hidden=true;
+            document.getElementById('A').hidden=false;
+            document.getElementById('B').hidden=false;
+            document.getElementById('C').hidden=false;
+            document.getElementById('D').hidden=false;
+            document.getElementById('E').hidden=false;
+            document.getElementById('F').hidden=false;
+            document.getElementById('G').hidden=false;
+            document.getElementById('H').hidden=false;
+            document.getElementById('I').hidden=false;
+            document.getElementById('J').hidden=false;
             alert('I dont know if you caught a snag or what but its coming out of your check');
-            //divOne = document.getElementById('fork1', 'fork2','knife1','knife2','plate1','plate2','jug','vase')
-            //divTwo = document.getElementByClass('X1', 'X2' , 'X3')
         }
     }
 }
 
 if (total >= 1000) {
     alert('YOU WON IT ALL, KID!');
-}
-
-function showTotal(total) {
-    ("You\'ve earned " + total + "points");
-
 };
